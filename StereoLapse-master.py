0c4838933ec049f32580d311fcd0e100e75dc707
alekszaf <a.zaforemska2@newcastle.ac.uk>
2021-05-07 20:54:10 +0100

    Some images contain no data

--- StereoLapse-master.py
+++ StereoLapse-master.py
@@ -16,13 +16,13 @@ GPIO.setup(channel, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 #GPIO.add_event_detect(button, GPIO.RISING, bouncetime=300)
 GPIO.add_event_detect(channel, GPIO.RISING)
 
-i = 0
+#i = 0
 
 while True:
     if GPIO.event_detected(channel):
         tstamp = datetime.now()
         camera.capture('/home/pi/Timelapse/orange_test_%s.png' %tstamp)
         print(f'Rising edge @{datetime.now()}')
-        i += 1
-        if i == 10:
-            break
+#        i += 1
+#        if i == 10:
+#            break
