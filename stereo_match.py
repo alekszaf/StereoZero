d5fe5f3712a90d6c1f4ea5d138ec11a237fee0e7
Aleksandra Zaforemska <a.zaforemska2@newcastle.ac.uk>
2021-08-24 23:58:21 +0100

    Load rectified images

---
+++ stereo_match.py
@@ -0,0 +1,6 @@
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+imgL = cv.imread('C://Temp//Timelapse//Right_recalibrated_test1//green_calib2021-08-21 14-37-08.431483.png', 0)
+imgR = cv.imread('C://Temp//Timelapse//Left_recalibrated_test1//orange_calib2021-08-21 14-37-08.771781', 0)
