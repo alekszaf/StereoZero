367cfa664989876bc9cb191e8ae8fd08ad8622ec
alekszaf <a.zaforemska2@newcastle.ac.uk>
2021-11-23 20:53:56 +0000

    Remove the redundant scripts

--- StereoLapse-master.py
+++ StereoLapse-master.py
@@ -3,29 +3,33 @@
 import RPi.GPIO as GPIO
 from datetime import datetime
 from picamera import PiCamera
-#from gpiozero import Button
 from time import sleep
 
+#Initialize the camera
 camera = PiCamera()
 
+#Set timer input GPIO
 channel = 4
 
-#button = Button(17)
+#Set button GPIO
+button = 17
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-#GPIO.setup(button, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(button, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
+GPIO.add_event_detect(channel, GPIO.RISING, bouncetime=100)
 #GPIO.add_event_detect(button, GPIO.RISING, bouncetime=300)
-GPIO.add_event_detect(channel, GPIO.RISING)
+GPIO.add_event_detect(button, GPIO.FALLING, bouncetime=300)
 
-i = 0
+timelapse_on = False
 
 while True:
-    if GPIO.event_detected(channel):
+    if GPIO.event_detected(button):
+        timelapse_on = not timelapse_on
+        
+    if GPIO.event_detected(channel) and timelapse_on:
         tstamp = datetime.now()
-        #camera.capture('/home/pi/Timelapse/orange_test_%s.png' %tstamp)
         print(f'Rising edge @{datetime.now()}')
-        i += 1
-        if i == 5:
-           break
+        camera.capture('/home/pi/Timelapse/orange_10cm_%s.png' %tstamp)
+
