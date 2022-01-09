39b6235c80c69e161732e6fbdee236d2832766ea
alekszaf <a.zaforemska2@newcastle.as.uk>
2022-01-09 18:58:29 +0100

    set chessboard size to 20mm

--- ImageProcessing/stereo_calibration.py
+++ ImageProcessing/stereo_calibration.py
@@ -15,7 +15,7 @@ criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 objp = np.zeros((chessboardSize[0] * chessboardSize[1], 3), np.float32)
 objp[:, :2] = np.mgrid[0:chessboardSize[0], 0:chessboardSize[1]].T.reshape(-1, 2)
 
-objp = objp * 30
+objp = objp * 20
 print(objp)
 
 # Store object points and image points
