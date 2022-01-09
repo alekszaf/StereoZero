459227fec17e892a2fa7fd77506452d05de77a9d
alekszaf <a.zaforemska2@newcastle.as.uk>
2022-01-09 20:02:05 +0100

    add stereoSGBM

--- ImageProcessing/stereo_match.py
+++ ImageProcessing/stereo_match.py
@@ -14,8 +14,9 @@ stereoMapR_y = cv_file.getNode('stereoMapR_y').mat()
 imgL = cv.imread('C:\\Users\\b7079552\\OneDrive - Newcastle University\\PhD\\Camera timelapses\\Zweibrucken_09-01-2022\\Left\\4m\\left_2022-01-09 15-24-05.838816.png', 1)
 imgR = cv.imread('C:\\Users\\b7079552\\OneDrive - Newcastle University\\PhD\\Camera timelapses\\Zweibrucken_09-01-2022\\Right\\4m\\right_2022-01-09 15-24-05.839931.png', 1)
 
-cv.imshow(imgL)
-cv.imshow(imgR)
+cv.imshow('Left', imgL)
+cv.imshow('Right', imgR)
+cv.waitKey(0)
 
 #Load sample images
 #imgR = cv.imread('C://Temp//stereo_samples//Yeuna9x.png')
@@ -25,21 +26,33 @@ grayLeft = cv.cvtColor(imgL, cv.COLOR_BGR2GRAY)
 grayRight = cv.cvtColor(imgR, cv.COLOR_BGR2GRAY)
 
 # Rectify the images
-grayLeft = cv.remap(grayLeft, stereoMapL_x, stereoMapL_y, cv.INTER_LINEAR, cv.BORDER_CONSTANT, 0)
-grayRight = cv.remap(grayRight, stereoMapR_x, stereoMapR_y, cv.INTER_LINEAR, cv.BORDER_CONSTANT, 0)
-cv.imshow('Left camera - stereo rectified', grayLeft)
-cv.imshow('Right camera - stereo rectified', grayRight)
-cv.waitKey(0)
+#grayLeft = cv.remap(grayLeft, stereoMapL_x, stereoMapL_y, cv.INTER_LINEAR, cv.BORDER_CONSTANT, 0)
+#grayRight = cv.remap(grayRight, stereoMapR_x, stereoMapR_y, cv.INTER_LINEAR, cv.BORDER_CONSTANT, 0)
+#cv.imshow('Left camera - stereo rectified', grayLeft)
+#cv.imshow('Right camera - stereo rectified', grayRight)
+#cv.waitKey(0)
 
-minDisparity = 0
-numDisparities=64
+#Calculate the disparity map
+minDisp = 0
+numDisp = 64
 
-stereo = cv.StereoBM_create(numDisparities=64, blockSize = 11)
-disparity = stereo.compute(grayLeft, grayRight)
+#StereoSGBM
+stereo = cv.StereoSGBM_create(minDisparity = minDisp,
+ numDisparities = numDisp,
+ blockSize = 15,
+ uniquenessRatio = 5,
+ speckleWindowSize = 5,
+ speckleRange = 5,
+ disp12MaxDiff = 1)
+
+#StereoBM
+#stereo = cv.StereoBM_create(numDisparities=numDisp, blockSize = 11)
+
+disparity = stereo.compute(imgL, imgR)
 
 #Scale down disparity values and normalize
-disparity = disparity.astype(np.float32)
-disparity = (disparity/16.0 - minDisparity)/numDisparities
+#disparity = disparity.astype(np.float32)
+#disparity = (disparity/16.0 - minDisp)/numDisp
 
 plt.imshow(disparity, 'gray')
 plt.show()
