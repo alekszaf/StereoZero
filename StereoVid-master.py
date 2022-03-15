9973fab66ca5c014e0665ef732f83a9373963e8c
alekszaf <a.zaforemska2@newcastle.as.uk>
2022-03-15 17:24:36 +0000

    Add comments for clarity

--- StereoVid-master.py
+++ StereoVid-master.py
@@ -19,18 +19,28 @@ while True:
         video_on = not video_on
         
     if video_on:
+        
+        #Initialize the camera with parameters of choice
         camera = PiCamera()
         camera.resolution = (640, 480)
         camera.awb_mode = 'off'
         camera.awb_gains = (1.1, 1.1)
         camera.iso = 200
-        tstamp = datetime.now()
         camera.shutter_speed = 10000
         camera.framerate = 20
+        
+        #Create the video file and start recording
+        tstamp = datetime.now()
         camera.start_recording('/home/pi/Timelapse/video_%s.h264' %tstamp)
         print(f'Recording started @{datetime.now()}')
+        
+        #Record for a given period of time
         camera.wait_recording(20)
         print(f'Recording complete @{datetime.now()}')
         camera.stop_recording()
+        
+        #Shut down the camera
         camera.close()
-        sleep(60)
+        
+        #Wait till the next capture
+        sleep(600)
