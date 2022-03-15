4ee04a82271dae042097f7dda3b4f56aa49adc39
alekszaf <a.zaforemska2@newcastle.ac.uk>
2022-03-15 15:21:45 +0000

    Enable on/off of the timelapse with button

--- StereoLapse-master.py
+++ StereoLapse-master.py
@@ -9,17 +9,16 @@ from time import sleep
 #camera = PiCamera()
 
 #Set timer input GPIO
-channel = 4
+channel = 5
 
 #Set button GPIO
-button = 17
+button = 6
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(button, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
 GPIO.add_event_detect(channel, GPIO.RISING, bouncetime=100)
-#GPIO.add_event_detect(button, GPIO.RISING, bouncetime=300)
 GPIO.add_event_detect(button, GPIO.FALLING, bouncetime=300)
 
 timelapse_on = False
@@ -30,8 +29,13 @@ while True:
         
     if GPIO.event_detected(channel) and timelapse_on:
         camera = PiCamera()
+        #camera.resolution = (2592, 1944)
+        camera.resolution = (720, 480)
+        camera.awb_mode = 'off'
+        camera.awb_gains = (1.1, 1.1)
+        camera.iso = 200
         tstamp = datetime.now()
         print(f'Rising edge @{datetime.now()}')
-        camera.capture('/home/pi/Timelapse/left_80cm_%s.png' %tstamp)
+        camera.capture('/home/pi/Timelapse/left_%s.png' %tstamp)
         camera.close()
 
