2c377d5b0815eabfb927891cb2e7d2068c06f86c
alekszaf <a.zaforemska2@newcastle.ac.uk>
2022-03-15 15:36:13 +0000

    Take a 30 second video on button press

---
+++ StereoVid-master.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+from datetime import datetime
+from picamera import PiCamera
+from time import sleep
+
+#Set button GPIO
+button = 6
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(button, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.add_event_detect(button, GPIO.FALLING, bouncetime=300)
+
+video_on = False
+
+while True:
+    if GPIO.event_detected(button):
+        video_on = not video_on
+        
+    if video_on:
+        camera = PiCamera()
+        camera.resolution = (640, 480)
+        camera.awb_mode = 'off'
+        camera.awb_gains = (1.1, 1.1)
+        camera.iso = 200
+        camera.start_recording('/home/pi/Timelapse/video', format='mjpeg')
+        print(f'Recording started @{datetime.now()}')
+        camera.wait_recording(30)
+        print(f'Recording complete @{datetime.now()}')
+        camera.stop_recording()
+        camera.close()
