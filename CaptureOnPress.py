fa9b58c0e6616ba67c6618dd38632f6cf6f8cf94
alekszaf <alzaforemska@gmail.com>
2022-08-21 20:55:15 +0100

    Print out the frame number

--- CaptureOnPress.py
+++ CaptureOnPress.py
@@ -7,9 +7,9 @@ button = Button(18)
 camera = PiCamera()
 camera.resolution = (3280, 2464)
 camera.awb_mode = 'off'
-camera.awb_gains = (1.0, 1.0)
+camera.awb_gains = (1.2, 1.3)
 camera.iso = 200
-#camera.start_preview(fullscreen=False, window=(100,20,640,480))
+camera.start_preview(fullscreen=False, window=(100,20,640,480))
 
 frame = 1
 
@@ -17,6 +17,7 @@ while True:
     try:
         button.wait_for_press()
         camera.capture('/home/pi/Timelapse/frame%03d.png' % frame)
+        print(frame)
         frame += 1
     except KeyboardInterrupt:
         camera.stop_preview()
