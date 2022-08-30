a2bf21ac68ed3e0f0733707448b69bc9c77464c5
alekszaf <alzaforemska@gmail.com>
2022-08-30 16:12:14 +0100

    Resolution modes

--- CaptureOnPress.py
+++ CaptureOnPress.py
@@ -8,11 +8,11 @@ camera = PiCamera()
 
 #Select resolution mode
 #camera.resolution = (1920, 1080) # mode 1
-#camera.resolution = (3280, 2464) #mode 2&3
+camera.resolution = (3280, 2464) #mode 2&3
 #camera.resolution = (1640, 1232) # mode 4
 #camera.resolution = (1640, 922) # mode 5
 #camera.resolution = (1280, 720) # mode 6
-camera.resolution = (640, 480) #mode 7
+#camera.resolution = (640, 480) #mode 7
 
 camera.awb_mode = 'off'
 camera.awb_gains = (1.2, 1.3)
