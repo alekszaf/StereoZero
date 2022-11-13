bb5c056b4f141da9d892560139ad0f178bb1e1d4
alekszaf <a.zaforemska2@newcastle.as.uk>
2022-11-13 21:31:58 +0000

    Add comments

--- Utils/fileRename.py
+++ Utils/fileRename.py
@@ -1,22 +1,26 @@
 import os
 
-folder = r'C:/Users/b7079552/OneDrive - Newcastle University/PhD/Camera calibration/2022/Aug22/R_b10_6-08-22/'
-count = 1
-# count increase by 1 in each iteration
+folder = r'C:/Users/b7079552/OneDrive - Newcastle University/PhD/Data/22-06-30-Cactus/Images/R_b24_3280x2464_CACT/'
+count = 40
+
 # iterate all files from a directory
 for file_name in os.listdir(folder):
+    
     # Construct old file name
     source = folder + file_name
 
     # Adding the count to the new file name and extension
-    destination = folder + "frame_" + str(count) + ".png"
+    destination = folder + "L_b24_d" + str(count) + "_m2.png"
 
     # Renaming the file
     os.rename(source, destination)
-    count += 1
+    
+    # Increase the count by 10
+    count += 10
+    
 print('All Files Renamed')
 
 print('New Names are')
-# verify the result
+# Verify the result
 res = os.listdir(folder)
 print(res)
