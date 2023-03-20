18058bcfb8e43286f4f1ef4cfa342be2119cf6fa
alekszaf <a.zaforemska2@newcastle.as.uk>
2023-03-20 17:55:01 +0000

    Establish path for import and export

--- ImageProcessing/edgeDetection.py
+++ ImageProcessing/edgeDetection.py
@@ -1,7 +1,8 @@
 import cv2
- 
+
 # Read the original image
-img = cv2.imread('test.jpg') 
+path = 'C:/Users/b7079552/OneDrive - Newcastle University/PhD/Data/23-02-17-ExhibitionPark/L3/1640x1232/'
+img = cv2.imread(f'{path}R_b120_d600_m4_l3.png')
 # Display original image
 cv2.imshow('Original', img)
 cv2.waitKey(0)
@@ -11,7 +12,7 @@ cv2.waitKey(0)
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # Blur the image for better edge detection
 img_blur = cv2.GaussianBlur(img_gray, (3,3), 0) 
- 
+
 # Sobel Edge Detection
 sobelx = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5) # Sobel Edge Detection on the X axis
 sobely = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5) # Sobel Edge Detection on the Y axis
@@ -24,12 +25,14 @@ cv2.imshow('Sobel Y', sobely)
 cv2.waitKey(0)
 cv2.imshow('Sobel X Y using Sobel() function', sobelxy)
 cv2.waitKey(0)
- 
+
 # Canny Edge Detection
-edges = cv2.Canny(image=img_blur, threshold1=100, threshold2=200)
+edges = cv2.Canny(image=img_blur, threshold1=50, threshold2=300)
 
 # Display Canny Edge Detection Image
 cv2.imshow('Canny Edge Detection', edges)
 cv2.waitKey(0)
- 
+
+cv2.imwrite(f'{path}R_edges.png', edges)
+
 cv2.destroyAllWindows()
